0853d4801dfdb51df1763c26603a3eac8a1e5c46
Cliff <>
2020-05-27 22:48:42 +0300

    кнопки - доработки по прерываниям, т.к. там есть косяки по работе переменных

--- src/button.cpp
+++ src/button.cpp
@@ -5,21 +5,32 @@
 /* ------------------------------------------------------------------------------------------- *
  * Инициализация кнопок
  * ------------------------------------------------------------------------------------------- */
-static btn_t btnall[] = {
+static volatile btn_t btnall[] = {
     { BUTTON_PIN_UP,    BTN_UP },
     { BUTTON_PIN_SEL,   BTN_SEL },
     { BUTTON_PIN_DOWN,  BTN_DOWN },
 };
 
-static std::vector<button_hnd_t> hndall;
+static volatile button_hnd_t hndlast = NULL;
 
-static uint8_t _state = 0;
-static uint32_t _statelast = 0;
+static volatile uint8_t _state = 0;
+static volatile uint32_t _statelast = 0;
+// this variable will be changed in the ISR (interrupt), and Read in main loop
+// static: says this variable is only visible to function in this file, its value will persist, it is a global variable
+// volatile: tells the compiler that this variables value must not be stored in a CPU register, it must exist
+//   in memory at all times.  This means that every time the value of intTriggeredCount must be read or
+//   changed, it has be read from memory, updated, stored back into RAM, that way, when the ISR 
+//   is triggered, the current value is in RAM.  Otherwise, the compiler will find ways to increase efficiency
+//   of access.  One of these methods is to store it in a CPU register, but if it does that,(keeps the current
+//   value in a register, when the interrupt triggers, the Interrupt access the 'old' value stored in RAM, 
+//   changes it, then returns to whatever part of the program it interrupted.  Because the foreground task,
+//   (the one that was interrupted) has no idea the RAM value has changed, it uses the value it 'know' is 
+//   correct (the one in the register).  
 
 /* ------------------------------------------------------------------------------------------- *
  * Проверка состояния
  * ------------------------------------------------------------------------------------------- */
-void btnChkState(btn_t &b) {
+void IRAM_ATTR btnChkState(volatile btn_t &b) {
     uint8_t val = digitalRead(b.pin);
     uint32_t m = millis();
     uint32_t tm = m-b.lastchg;
@@ -33,11 +44,11 @@ void btnChkState(btn_t &b) {
         if (pushed) {
             if (((b.pushed & BTN_PUSHED_SIMPLE) == 0) && ((val != LOW) || (b.hndlong == NULL))) {
                 b.pushed |= BTN_PUSHED_SIMPLE;
-                if (b.hndsmpl != NULL) hndall.push_back(b.hndsmpl); // Простое нажатие
+                hndlast = b.hndsmpl; // Простое нажатие
             }
             if (((b.pushed & BTN_PUSHED_LONG) == 0) && ((m-b.lastchg) >= BTN_LONG_TIME)) {
                 b.pushed |= BTN_PUSHED_LONG | BTN_PUSHED_SIMPLE; // чтобы после длинного нажатия не сработало простое, помечаем простое тоже сработавшим
-                if (b.hndlong != NULL) hndall.push_back(b.hndlong); // Длинное нажатие
+                hndlast = b.hndlong; // Длинное нажатие
             }
         }
     }
@@ -54,6 +65,9 @@ void btnChkState(btn_t &b) {
         _statelast = m;
     }
 }
+void IRAM_ATTR btnChkState0() { btnChkState(btnall[0]); }
+void IRAM_ATTR btnChkState1() { btnChkState(btnall[1]); }
+void IRAM_ATTR btnChkState2() { btnChkState(btnall[2]); }
 
 /* ------------------------------------------------------------------------------------------- *
  * Инициализация - включаем пины и прописываем прерывания
@@ -66,17 +80,17 @@ void btnInit() {
     
     attachInterrupt(
         digitalPinToInterrupt(btnall[0].pin),
-        []() { btnChkState(btnall[0]); },
+        btnChkState0,
         CHANGE
     );
     attachInterrupt(
         digitalPinToInterrupt(btnall[1].pin),
-        []() { btnChkState(btnall[1]); },
+        btnChkState1,
         CHANGE
     );
     attachInterrupt(
         digitalPinToInterrupt(btnall[2].pin),
-        []() { btnChkState(btnall[2]); },
+        btnChkState2,
         CHANGE
     );
 }
@@ -87,10 +101,10 @@ void btnInit() {
 void btnProcess() {
     for (auto &b : btnall)
         btnChkState(b);
-    if (hndall.size() > 0) {
-        for (auto hnd: hndall)
-            hnd();
-        hndall.clear();
+    
+    if (hndlast != NULL) {
+        hndlast();
+        hndlast = NULL;
     }
 }
 
@@ -108,7 +122,7 @@ void btnHndClear() {
  * Установка одного обработчика нажатия на кнопку
  * ------------------------------------------------------------------------------------------- */
 void btnHnd(btn_code_t btn, button_time_t tm, button_hnd_t hnd) {
-    btn_t *b;
+    volatile btn_t *b;
     switch (btn) {
         case BTN_UP:    b = &(btnall[0]); break;
         case BTN_SEL:   b = &(btnall[1]); break;
@@ -149,7 +163,7 @@ uint32_t btnPressed(uint8_t &btn) {
 void btnPush(btn_code_t btn, button_time_t tm) {
     _statelast = millis();
     
-    btn_t *b;
+    volatile btn_t *b;
     switch (btn) {
         case BTN_UP:    b = &(btnall[0]); break;
         case BTN_SEL:   b = &(btnall[1]); break;
