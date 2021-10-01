cca2386dcab60738ccab4d518b25af5461a50672
Cliff <>
2021-10-01 22:14:32 +0300

    доработано лого для hw3

--- src/view/logo.cpp
+++ src/view/logo.cpp
@@ -4,8 +4,16 @@
 #include "../cfg/main.h"
 
 void ViewLogo::draw(U8G2 &u8g2) {
-    u8g2.setFont(u8g2_font_open_iconic_other_8x_t);
-    u8g2.drawGlyph((u8g2.getDisplayWidth()-56)/2, (u8g2.getDisplayHeight()-35)/2 + 56/2, 0x40);
+#if HWVER == 4
+#define FONTNAME    u8g2_font_open_iconic_other_8x_t
+#define FONTSIZE    56
+#else
+#define FONTNAME    u8g2_font_open_iconic_other_4x_t
+#define FONTSIZE    28
+#endif
+    
+    u8g2.setFont(FONTNAME);
+    u8g2.drawGlyph((u8g2.getDisplayWidth()-FONTSIZE)/2, (u8g2.getDisplayHeight()-32)/2 + FONTSIZE/2, 0x40);
     
     char s[30];
     u8g2.setFont(u8g2_font_ImpactBits_tr);
