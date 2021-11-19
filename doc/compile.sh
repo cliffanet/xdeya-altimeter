faba6394c6c1302aa162ac38689a121eef684bff
Cliff <>
2021-11-20 01:44:03 +0300

    документация - скрипт генерации pdf и doc из md

--- doc/compile.sh
+++ doc/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pandoc userman/*.*.md -o userman.pdf --pdf-engine=xelatex --toc -N -M date="`date "+%e %B, %Y"`" --resource-path=userman --lua-filter fix-links-single-file.lua
+for f in userman/*.*.md; do cat "$f"; echo; echo "\\pagebreak"; done | pandoc -f markdown -o userman.pdf --pdf-engine=xelatex --toc -N -M date="`date "+%e %B, %Y"`" --resource-path=userman --lua-filter fix-links-single-file.lua
 
 pandoc userman/*.*.md -o userman.docx --toc -N -M date="`date "+%e %B, %Y"`" --resource-path=userman --reference-doc=style-ref.docx --lua-filter fix-links-single-file.lua
 
