e25c6ff1aa62f7c80dcb4184497ab4ad7a0e630c
Cliff <>
2022-04-10 23:00:37 +0300

    отображение жпс - при отсутствии компаса на странице "только жпс" рисовать полностью круг

--- src/view/mainnav.cpp
+++ src/view/mainnav.cpp
@@ -14,18 +14,43 @@
 #define PNT(x,y,ang,cx,cy)          static_cast<int>(round(cos(ang) * ((x) - (cx)) - sin(ang) * ((y) - (cy))) + (cx)),\
                                     static_cast<int>(round(sin(ang) * ((x) - (cx)) + cos(ang) * ((y) - (cy))) + (cy))
 
-#define ARG_COMP_DEF   U8G2 &u8g2, int cx, int cy, int w, int h
-#define ARG_COMP_CALL  u8g2, cx, cy, w, h
+static inline pnt_t pntinit(int cx, int cy, int rx, int ry, double ang) {
+    // "ang + PI/2"     - это смещение математического угла до географического
+    // "cy - ..."   - это разница между матиматической осью o-y (снизу вверх) и дисплейной (сверху вниз)
+    return {
+        static_cast<int>(cx + round(cos(ang+PI/2) * rx)),
+        static_cast<int>(cy - round(sin(ang+PI/2) * ry))
+    };
+}
 
+#define PNTINIT(dr)         pntinit(cx,cy, h-20+(dr),ry+(dr), head)
+#define PNTINIT2(dr,koef)   pntinit(cx,cy, (h-20+(dr)) * (koef), (ry+(dr)) * (koef), head)
+
+
+#define PNT2XY(pnt,dx,dy)   ((pnt).x+(dx)), ((pnt).y+(dy))
+#define P2XY                p.x, p.y
+
+#define ARG_COMP_DEF   U8G2 &u8g2, int cx, int cy, int w, int h, int ry
+#define ARG_COMP_CALL  u8g2, cx, cy, w, h, ry
+
+/*
 static void drawCircleSeg(U8G2 &u8g2, int x, int y, int cx, int cy) {
     u8g2.drawPixel(cx-x, cy-y);
     u8g2.drawPixel(cx-y, cy-x);
     u8g2.drawPixel(cx+x, cy-y);
     u8g2.drawPixel(cx+y, cy-x);
 }
+*/
 
 static void drawGrid(ARG_COMP_DEF) {
     u8g2.drawDisc(cx, cy, 2);
+    
+    if (compass().ok)
+        u8g2.drawEllipse(cx, cy, h-20, ry, U8G2_DRAW_UPPER_LEFT | U8G2_DRAW_UPPER_RIGHT);
+    else
+        u8g2.drawEllipse(cx, cy, h-20, ry);
+    /*
+    return;
     
     int rad = h-20;
     int
@@ -52,6 +77,7 @@ static void drawGrid(ARG_COMP_DEF) {
     
         drawCircleSeg(u8g2, x, y, cx, cy);
     }
+    */
 }
 
 static void drawText(ARG_COMP_DEF) {
@@ -121,7 +147,7 @@ static void drawText(ARG_COMP_DEF) {
             break;
     }
     y1 += 2+u8g2.getAscent();
-    u8g2.drawTxt(w-u8g2.getTxtWidth(s), y1, s);
+    u8g2.drawTxt(w-u8g2.getTxtWidth(s), compass().ok ? y1 : h-2, s);
     
     // запись трека
 #if HWVER < 4
@@ -133,14 +159,14 @@ static void drawText(ARG_COMP_DEF) {
         u8g2.drawGlyph(0, h-u8g2.getAscent(), 'R');
 }
 
-static void drawPntC(U8G2 &u8g2, int x, int y) {
+static void drawPntC(U8G2 &u8g2, const pnt_t &p) {
     u8g2.setDrawColor(0);
-    u8g2.drawDisc(x,y, 8);
+    u8g2.drawDisc(p.x,p.y, 8);
     u8g2.setDrawColor(1);
-    u8g2.drawDisc(x,y, 6);
+    u8g2.drawDisc(p.x,p.y, 6);
     u8g2.setFont(u8g2_font_helvB08_tr);
     u8g2.setDrawColor(0);
-    u8g2.drawGlyph(x-2,y+4, '0' + pnt.num());
+    u8g2.drawGlyph(p.x-2,p.y+4, '0' + pnt.num());
     u8g2.setDrawColor(1);
 }
 
@@ -157,25 +183,27 @@ static void drawPoint(ARG_COMP_DEF, double head = 0) {
     
     if (dist < 8) {
         // in point
-        drawPntC(u8g2, cx,cy);
+        drawPntC(u8g2, { cx,cy });
         u8g2.drawCircle(cx,cy, 10);
         u8g2.drawCircle(cx,cy, 15);
         return;
     }
     
-    double courseto =
+    head +=
         gpsCourse(
             GPS_LATLON(gps.lat),
             GPS_LATLON(gps.lon),
             pnt.cur().lat, 
             pnt.cur().lng
         ) * DEG_TO_RAD;
-    courseto += head;
     
-    int prad = dist > 400 ? h-20 : dist * (h-20) / 400;
-    drawPntC(u8g2, PNT(cx,cy-prad, courseto, cx,cy));
+    double koef = dist > 400 ? 1 : static_cast<double>(dist) / 400;
+    auto p = PNTINIT2(0, koef);
+    if (p.y > h-7) p.y = h-7;
+    drawPntC(u8g2, p);
 }
 
+/*
 static void drawMoveArr(ARG_COMP_DEF, int n = 0, double head = 0) {
     u8g2.drawTriangle(
 #if HWVER < 4
@@ -189,6 +217,7 @@ static void drawMoveArr(ARG_COMP_DEF, int n = 0, double head = 0) {
 #endif
     );
 }
+*/
 
 static void drawMove(ARG_COMP_DEF, double head = 0) {
     auto &gps = gpsInf();
@@ -198,6 +227,7 @@ static void drawMove(ARG_COMP_DEF, double head = 0) {
     
     if (speed < 0.5)
         return;
+    /*
     drawMoveArr(ARG_COMP_CALL, 0, head);
     if (speed < 5)
         return;
@@ -211,14 +241,66 @@ static void drawMove(ARG_COMP_DEF, double head = 0) {
     if (speed < 35)
         return;
     drawMoveArr(ARG_COMP_CALL, 4, head);
+    */
+    
+    pnt_t p;
+    
+    if (speed < 5) {
+        u8g2.drawDisc(cx,cy, 3);
+        p = PNTINIT2(0, 0.15);
+        u8g2.drawLine(P2XY, cx,cy);
+    }
+    else
+    if (speed < 15) {
+        u8g2.drawDisc(cx,cy, 4);
+        p = PNTINIT2(0, 0.1);
+        u8g2.drawDisc(P2XY, 3);
+        p = PNTINIT2(0, 0.20);
+        u8g2.drawLine(P2XY, cx,cy);
+    }
+    else
+    if (speed < 25) {
+        u8g2.drawDisc(cx,cy, 6);
+        p = PNTINIT2(0, 0.14);
+        u8g2.drawDisc(P2XY, 4);
+        p = PNTINIT2(0, 0.23);
+        u8g2.drawDisc(P2XY, 3);
+        p = PNTINIT2(0, 0.33);
+        u8g2.drawLine(P2XY, cx,cy);
+    }
+    else
+    if (speed < 35) {
+        u8g2.drawDisc(cx,cy, 9);
+        p = PNTINIT2(0, 0.19);
+        u8g2.drawDisc(P2XY, 6);
+        p = PNTINIT2(0, 0.32);
+        u8g2.drawDisc(P2XY, 4);
+        p = PNTINIT2(0, 0.41);
+        u8g2.drawDisc(P2XY, 3);
+        p = PNTINIT2(0, 0.54);
+        u8g2.drawLine(P2XY, cx,cy);
+    }
+    else {
+        u8g2.drawDisc(cx,cy, 13);
+        p = PNTINIT2(0, 0.25);
+        u8g2.drawDisc(P2XY, 9);
+        p = PNTINIT2(0, 0.43);
+        u8g2.drawDisc(P2XY, 6);
+        p = PNTINIT2(0, 0.56);
+        u8g2.drawDisc(P2XY, 4);
+        p = PNTINIT2(0, 0.66);
+        u8g2.drawDisc(P2XY, 3);
+        p = PNTINIT2(0, 0.76);
+        u8g2.drawLine(P2XY, cx,cy);
+    }
 }
 
 static void drawNavi(ARG_COMP_DEF, double head = 0) {
     auto &gps = gpsInf();
-
+    
     if (!GPS_VALID(gps)) {
         u8g2.setFont(u8g2_font_open_iconic_www_4x_t);
-        u8g2.drawGlyph(cx-16,cy+3, 'J');
+        u8g2.drawGlyph(cx-16, compass().ok ? cy+3 : cy+16, 'J');
         return;
     }
     
@@ -230,10 +312,8 @@ static void drawNavi(ARG_COMP_DEF, double head = 0) {
 }
 
 static void drawCompass(ARG_COMP_DEF, double head = 0) {
-    int rad = h-20;
-    pnt_t p;
+    pnt_t p = PNTINIT(6);
     
-    p = { PNT(cx,cy-rad-6, head, cx,cy) };
     if (p.y > h-7) p.y = h-7;
     u8g2.setDrawColor(0);
     u8g2.drawDisc(p.x, p.y, 7);
@@ -257,12 +337,15 @@ class ViewMainNav : public ViewMain {
         void draw(U8G2 &u8g2) {
             int w = u8g2.getDisplayWidth();
             int h = u8g2.getDisplayHeight();
-            int cx = w/2-1, cy = h-12;
+            int cx = w/2-1,
+                cy = compass().ok ?     h-12    :   h/2-1,
+                ry = compass().ok ?     h-20    :   h/2-10;
             
             drawGrid(ARG_COMP_CALL);
+            
             drawText(ARG_COMP_CALL); // Весь текст пишем в самом начале, чтобы графика рисовалась поверх
             
-            drawCompass(ARG_COMP_CALL, 2*PI - compass().head);
+            drawCompass(ARG_COMP_CALL, compass().head);
         }
 };
 static ViewMainNav vNav;
